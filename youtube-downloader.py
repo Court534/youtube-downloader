b8c3582cc1d3ba899e07491a1f0e2673d7421d18
Courtney Stow <courney-stow@hotmail.co.uk>
2022-11-05 17:29:46 +0000

    Finished code

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -4,5 +4,7 @@ from sys import argv
 link = argv[1]
 yt = YouTube(link)
 
-print("Title: ", yt.title)
-print("Number of views: ", yt.views)
+print("Now downloading:", yt.title)
+
+yd = yt.streams.get_highest_resolution() 
+yd.download("C:/Users/CourtneyStow/Documents/Youtube-downloads/")
